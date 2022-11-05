5cacc7a26d55feb07476dacd007cc34d3a5c8dc9
Honglin Cui <hlcuicn@gmail.com>
2022-11-05 18:56:08 +0800

    working

--- force-app/main/default/lwc/caSearchForm/caSearchForm.js
+++ force-app/main/default/lwc/caSearchForm/caSearchForm.js
@@ -1,9 +1,9 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
-import ACCOUNT_OBJECT from '@salesforce/schema/Account'
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import TYPE_FIELD from '@salesforce/schema/Account.Type';
 export default class CaSearchForm extends LightningElement {
-    
+
     @wire(getObjectInfo, {objectApiName: ACCOUNT_OBJECT})
     acObjInfo;
     error = undefined;
@@ -12,11 +12,11 @@ export default class CaSearchForm extends LightningElement {
     @track
     searchOptions;
     // account type combobox options
-    @wire(getPicklistValues, { recordTypeId: '$accountInfo.data.defaultRecordTypeId', fieldApiName: TYPE_FIELD})
-    acTypes({data, error}) {
+    @wire(getPicklistValues, { recordTypeId: '$acObjInfo.data.defaultRecordTypeId', fieldApiName: TYPE_FIELD})
+    accountTypes({data, error}) {
         if (data) {
-            this.searchOptions = data.map( type => {
-                return {label: type.label, value: type.value};
+            this.searchOptions = data.values.map( type => {
+                return { label: type.label, value: type.value };
             });
             this.searchOptions.unshift({label: 'All Types', value: ''});
             this.error = undefined;
@@ -28,8 +28,8 @@ export default class CaSearchForm extends LightningElement {
     // search accounts when type changes.
     handleCmbTypeChange(event) {
         this.selectedTypeId = event.detail.value;
-        const searchEvent = new CustomEvent('searchAccount', {
-            detail: selectedTypeId,
+        const searchEvent = new CustomEvent('search', {
+            detail: { acTypeId: this.selectedTypeId },
         });
         this.dispatchEvent(searchEvent);
     }
