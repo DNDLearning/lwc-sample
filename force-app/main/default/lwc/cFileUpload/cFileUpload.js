19d98e694bddb8bd0b080a00a826aa6981a45d2c
Honglin Cui <hlcuicn@gmail.com>
2022-11-15 21:05:38 +0800

    Add file download.

--- force-app/main/default/lwc/cFileUpload/cFileUpload.js
+++ force-app/main/default/lwc/cFileUpload/cFileUpload.js
@@ -1,15 +1,40 @@
 import { api, LightningElement, wire } from 'lwc';
 import { subscribe, unsubscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 import fetchFiles from '@salesforce/apex/FileuploadControl.fetchFiles';
 import { refreshApex } from '@salesforce/apex';
 
 import CAMC from '@salesforce/messageChannel/CuiAccountMessageChannel__c';
 
-export default class CFileUpload extends LightningElement {
+export default class CFileUpload extends NavigationMixin( LightningElement ) {
 
     columns = [
-        { label: 'Title', fieldName: 'Title' },
+        { label: 'Title', fieldName: 'fileUrl', type: 'url',
+            typeAttributes: { label: { fieldName: 'Title' } },
+            sortable: true,
+        },
+        { type: 'button', 
+            typeAttributes: {
+                iconName: 'utility:download',
+                label: 'download',
+                name: 'download',
+                disabled: false,
+                value: 'download',
+                variant: 'brand'
+            }
+        },
+        { type: 'button', 
+            typeAttributes: {
+                iconName: 'utility:search',
+                label: 'preview',
+                name: 'preview',
+                disabled: false,
+                value: 'preview',
+                variant: 'brand'
+            }
+        },
+
     ];
 
     recId;
@@ -34,6 +59,10 @@ export default class CFileUpload extends LightningElement {
         return this.recordId == undefined;
     }
 
+    get url() {
+        return '/sfc/servlet.shepherd/document/download/';
+    }
+
     @wire(MessageContext)
     messageContext;
 
@@ -47,7 +76,11 @@ export default class CFileUpload extends LightningElement {
             console.log('file uplaod files for recordId : ' + this.recordId);
             console.log(data);
             this.fileNames = data.map( cd => {
-                return { Title : cd.ContentDocument.Title };
+                return { 
+                    Id: cd.ContentDocumentId,
+                    Title: cd.ContentDocument.Title ,
+                    fileUrl: this.url + cd.ContentDocumentId,
+                };
             });
             this.error = undefined;
         }
@@ -94,5 +127,32 @@ export default class CFileUpload extends LightningElement {
 
     async refresh() {
         await refreshApex(this.fetchResults);
+    }
+
+    handleRowAction(event) {
+        let actionName = event.detail.action.name;
+        let row = event.detail.row;
+        console.log(actionName);
+        console.log(row);
+
+        switch (actionName) {
+            case 'download':
+                window.open(row.fileUrl);
+                break;
+            case 'preview':
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__namedPage',
+                    attributes: {
+                        pageName: 'filePreview'
+                    },
+                    state: {
+                        selectedRecordId: row.Id
+                    }
+                })
+                break;
+            default:
+                break;
+        }
+
     }
 }
